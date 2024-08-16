d0cbb936c28ec2f9b8bbee39bef82bc25abbfd79
Mignon François <mignon.francois@gmail.com>
2024-08-16 21:03:35 +0200

    validation + ajout des sondage dans la DB

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -2,13 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Poll;
 use Livewire\Component;
+use Livewire\Attributes\Validate;
+
 
 class CreatePoll extends Component
 {
     // les variables public dans un componement livewire est accessible dans la vue correspondante
+    #[Validate('required|min:3|max:255')]
     public $title;
+    #[Validate('required|array|min:1|max:10')]
+    // #[Validate('required|min:1|max:255')]
     public $options = ['premier'];
+
+    
 
     public function render()
     {
@@ -19,6 +27,40 @@ class CreatePoll extends Component
     {
         // les crochet vide permettent d'ajouter un nouvel element
         $this->options[] = '';
+    }
+
+    public function removeOption($index)
+    {
+        unset($this->options[$index]);
+        //reindexer les valeurs
+        $this->options = array_values($this->options);
+    }
+
+    public function createPoll()
+    {
+        //appel de la validation
+        $this->validate();
+        /* #region V1 */
+        // $poll = Poll::create([
+        //     'title' => $this->title
+        // ]);
+
+        // foreach ($this->options as $optionName) {
+        //     $poll->options()->create(['name' => $optionName]);
+        // }
+        /* #endregion */
+
+        /* #region V2 avec moin de variable et plus de methode laravel */
+        Poll::create([
+            'title' => $this->title
+        ])->options()->createMany(
+            collect($this->options)
+                ->map(fn($option) => ['name' => $option])
+                ->all()
+        );
+        // apres soumission, on px reset les champs title et option
+        $this->reset(['title', 'options']);
+        /* #endregion */
     }
 
     // cette methode agit comme un constructeur
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,8 +1,11 @@
 <div>
-    <form>
+    <form wire:submit.prevent="createPoll">
         <label>Poll title</label>
         <input type="text" wire:model.live="title"/>
-        Titre actuel: {{$title}}
+        {{-- Titre actuel: {{$title}} --}}
+        @error('title')
+            <div class="text-red-500">{{$message}}</div>
+        @enderror
 
         <div class="mt-4 mb-4">
             {{-- prevent empeche le boutton d'envoyer un formulair, qui est le comportement par defaut d'un boutton html --}}
@@ -13,9 +16,18 @@
         <div >
             @foreach ($options as $index=>$option)
                 <div class="mb-4">
-                    {{$index}} - {{$option}}
+                    {{-- {{$index}} - {{$option}} --}}
+                    <label>Option {{$index +1}}</label>
+                    <div class="flex gap-2">
+                        <input type="text" wire:model.live="options.{{$index}}"/>
+                        @error('options.{{$index}}')
+                            <div class="text-red-500">{{$message}}</div>
+                         @enderror
+                        <button class="btn" wire:click.prevent="removeOption({{$index}})">Supprimer</button>
+                    </div>
                 </div>
             @endforeach
         </div>
+        <button type="submit" class="btn">Creer le sondage</button>
     </form>
 </div>
